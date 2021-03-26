32a538acd7c6b9618bf56bf47bb2b618440be10b
lindseyms <51429410+lindseyms@users.noreply.github.com>
2021-03-25 20:06:24 -0500

    Completed EmployerController, implemented SkillController

--- queries.sql
+++ queries.sql
@@ -2,6 +2,7 @@
 columns: id (int), employer (varchar(255)), name (varchar(255)), skills (varchar(255));
 
 ## Part 2: Test it with SQL
+SELECT * FROM employer WHERE location = 'St. Louis City';
 
 ## Part 3: Test it with SQL
 
