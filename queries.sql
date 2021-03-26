ebfa6bab5ed124f7ced815251443ff24aa4772f6
lindseyms <51429410+lindseyms@users.noreply.github.com>
2021-03-26 18:55:29 -0500

    Implemented list and search functionality

--- queries.sql
+++ queries.sql
@@ -8,3 +8,4 @@ SELECT * FROM employer WHERE location = 'St. Louis City';
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT skill.name, skill.description FROM skill INNER JOIN job_skills WHERE skill.id = job_skills.skills_id AND job_skills.jobs_id IS NOT NULL ORDER BY skill.name ASC;
